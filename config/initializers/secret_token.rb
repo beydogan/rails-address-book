dd5c1bcb92b659f7db87bd5b8612225efff32261
Mehmet Beydogan <mbbrain@gmail.com>
2014-05-03 16:03:43 +0300

    rails new address_book

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AddressBook::Application.config.secret_key_base = '924497b748a7800cbd84a84c75c42a21f741757f272f89eae8fe52c27496a259b6966257555a872ee291f17d33785876187f208df9525b30c4d2064744a1e255'
