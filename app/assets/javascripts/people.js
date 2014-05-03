8e3d15a9c55721e1d57abb6254de214622981ddc
Mehmet Beydogan <mbbrain@gmail.com>
2014-05-03 16:56:06 +0300

    adds address search and adding marker to map feature

--- app/assets/javascripts/people.js
+++ app/assets/javascripts/people.js
@@ -1,2 +1,51 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+window.map = null;
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var mapOptions = {
+        center: new google.maps.LatLng(-34.397, 150.644),
+        zoom: 8
+    };
+    map = new google.maps.Map(document.getElementById("map-canvas"),
+        mapOptions);
+
+    if($('#person-address') && $('#person-address').val() != ""){ //Update map when address field is not empty
+        decodeAddress($('#person-address').val());
+    }
+}
+
+
+/*
+ Find location of given address and adds marker to map.
+ */
+function decodeAddress(address) {
+    geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+
+            marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+        } else {
+
+        }
+    });
+}
+
+function removeMarker(){
+    marker.setMap(null);
+}
+
+$(document).ready(function(){
+    initialize();
+
+    $('#person-address').on('input', function() {
+        removeMarker()
+        decodeAddress($(this).val());
+    })
+
+
+})
