3a0f05b7967ec63f0ee33a5b936a24e9f51df831
Mehmet Beydogan <mbbrain@gmail.com>
2014-05-03 16:04:45 +0300

    adds scaffold person model

---
+++ app/views/people/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @person, :id, :name, :surname, :address, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 AddressBook::Application.routes.draw do
+  resources :people
+
+  root "people#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
